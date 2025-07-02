bad96d29b4a719358d1440f9e6d48b9297869305
Ma'sum <mclasix@gmail.com>
2025-07-02 10:35:20 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('door-prize');
+    return Inertia::render('doorprize');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
